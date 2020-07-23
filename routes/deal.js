4f01aed40a2502e952f0008fc1aad4493ef0635a
filiafobico <oliveira1eduardo2luis3@gmail.com>
2020-07-23 19:41:06 -0300

    simple add or update deal on mongodb

--- routes/deal.js
+++ routes/deal.js
@@ -1,7 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 router.post('/', (req, res, next) => {
+  const deal = {
+    _id: req.body.current.id,
+    title: req.body.current.title,
+    value: req.body.current.value,
+    status: req.body.current.status,
+    date: new Date()
+  };
+
+  global.db
+    .collection('pipedrive-deal')
+    .updateOne(
+      { _id: deal._id },
+      { $set: deal },
+      { upsert: true }
+    );
+
   res.sendStatus(200);
 });
 
